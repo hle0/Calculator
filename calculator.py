1315ed19a13860057bd2ff63515535965e2a0c13
hle0 <91701075+hle0@users.noreply.github.com>
2022-02-07 16:22:15 -0500

    add multiplication and division options
    Signed-off-by: hle0 <91701075+hle0@users.noreply.github.com>

--- calculator.py
+++ calculator.py
@@ -3,11 +3,15 @@
 a = int(input('First  number = '))
 b = int(input('Second number = '))
 
-op = input('Add or subtract? (+/-) ')
+op = input('Addition, subtraction, multiplication, or division? (+|-|*|/) ')
 
 if op == '+':
 	print(f'Sum = {a+b}')
 elif op == '-':
 	print(f'Difference = {a-b}')
+elif op == '*':
+	print(f'Product = {a*b}')
+elif op == '/':
+	print(f'Quotient = {a/b}')
 else:
 	raise ValueError('invalid operator')
