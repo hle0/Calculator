b4c6bc89003a745349bcc2da4503eeacdb73dc89
hle0 <91701075+hle0@users.noreply.github.com>
2022-02-07 16:18:15 -0500

    add subtraction option and ask user for operator
    Signed-off-by: hle0 <91701075+hle0@users.noreply.github.com>

--- calculator.py
+++ calculator.py
@@ -3,4 +3,11 @@
 a = int(input('First  number = '))
 b = int(input('Second number = '))
 
-print(f'Sum = {a+b}')
+op = input('Add or subtract? (+/-) ')
+
+if op == '+':
+	print(f'Sum = {a+b}')
+elif op == '-':
+	print(f'Difference = {a-b}')
+else:
+	raise ValueError('invalid operator')
